a455b72e2b145664d50687c59a7917f264da3a0b
AnantSingh08 <69403529+AnantSingh08@users.noreply.github.com>
2026-09-05 02:08:42 +0530

    Add: k-closest-points-to-origin - submission-2

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-2.java
@@ -0,0 +1,12 @@
+// Brute Force sorting
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        Arrays.sort(points, (a,b) -> Integer.compare((a[0] * a[0] + a[1] * a[1]),b[0] * b[0] + b[1] * b[1]));
+
+        int res[][] = new int[k][2];
+        for(int i = 0; i<k; i++) {
+            res[i] = points[i];
+        }
+        return res;
+    }
+}
