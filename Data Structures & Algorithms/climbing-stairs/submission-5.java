3262063737238fcbff4f2c1d85bf594d7ff5001d
AnantSingh08 <69403529+AnantSingh08@users.noreply.github.com>
2026-09-05 03:53:21 +0530

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.java
@@ -0,0 +1,16 @@
+// Space optimisation
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=2) return n;
+
+        int step1 = 1;
+        int step2 = 2;
+        
+        for(int i=3; i <=n; i++) {
+            int sum = step1+step2;
+            step1=step2;
+            step2=sum;
+        }
+        return step2;
+    }
+}
