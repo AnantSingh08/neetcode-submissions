89fe589f8602a212e689cbd47b3ae72b2ab11d37
AnantSingh08 <69403529+AnantSingh08@users.noreply.github.com>
2026-09-07 03:07:23 +0530

    Add: maximum-subarray - submission-8

---
+++ Data Structures & Algorithms/maximum-subarray/submission-8.java
@@ -0,0 +1,15 @@
+// Tabulation
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max = nums[0];
+        int[] dp = new int[nums.length];
+        if(nums.length<=1) return nums.length==0?-1:nums[0];
+
+        dp[0] = nums[0];
+        for(int i=1; i<nums.length; i++) {
+            dp[i] = Math.max(nums[i], dp[i-1]+nums[i]);
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+}
